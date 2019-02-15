80b76f372c37bae500de42fd3da2db476f79204b
TheoLeyenda <theoleyenda1997@gmail.com>
2019-02-15 14:19:08 -0300

    Menu de inicio programado

--- Snake-Clon_TheoLeyenda/Assets/Scripts/Menu/MainMenu.cs
+++ Snake-Clon_TheoLeyenda/Assets/Scripts/Menu/MainMenu.cs
@@ -5,8 +5,33 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour {
 
+    public GameObject mainMenu;
+    public GameObject creditsMenu;
+    private void Start()
+    {
+        creditsMenu.SetActive(false);
+        mainMenu.SetActive(true);
+    }
     public void Play()
     {
         SceneManager.LoadScene("Pantalla de carga");
+    }
+    public void Credits()
+    {
+        mainMenu.SetActive(false);
+        creditsMenu.SetActive(true);
+
+    }
+    public void BackMenu()
+    {
+        creditsMenu.SetActive(false);
+        mainMenu.SetActive(true);
+    }
+    public void ExitGame()
+    {
+#if UNITY_EDITOR
+            UnityEditor.EditorApplication.isPlaying = false;
+#endif
+            Application.Quit();
     }
 }
