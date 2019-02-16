0b4c392affab65038b9d94bf81d788fa9bb684f5
TheoLeyenda <theoleyenda1997@gmail.com>
2019-02-16 14:59:22 -0300

    Pantalla de GameOver(basica) Lista

---
+++ Snake-Clon_TheoLeyenda/Assets/Scripts/GameOver.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameOver : MonoBehaviour {
+
+    public void BackToMainMenu()
+    {
+        DataStructure.auxiliaryDataStructure.playerData.life = DataStructure.auxiliaryDataStructure.life;
+        DataStructure.auxiliaryDataStructure.playerData.score = 0;
+        DataStructure.auxiliaryDataStructure.seconds = 0;
+        DataStructure.auxiliaryDataStructure.minutes = 0;
+        DataStructure.auxiliaryDataStructure.minutesInLevel = 0;
+        DataStructure.auxiliaryDataStructure.secondsInLevel = 0;
+        DataStructure.auxiliaryDataStructure.nextLevel.level = 0;
+        SceneManager.LoadScene("SplashScreen");
+    }
+}
