a4ea1e5871b6d42db2bf84c9572efff6eb1fe278
TheoLeyenda <theoleyenda1997@gmail.com>
2019-02-18 20:00:36 -0300

    Obstaculo 1 - Auto LISTO

---
+++ Snake-Clon_TheoLeyenda/Assets/Scripts/Obstaculos/Car.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Car : MonoBehaviour {
+
+    // Use this for initialization
+    public float speed;
+    public float angleRotation;
+    public float diley;
+    private float auxDiley;
+	void Start () {
+        auxDiley = diley;
+        diley = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Movement();
+	}
+    public void Movement()
+    {
+        transform.position = transform.position + transform.up * Time.deltaTime * speed;
+        if(diley > 0)
+        {
+            diley = diley - Time.deltaTime;
+        }
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.tag == "punto curva")
+        {
+            if (diley <= 0)
+            {
+                transform.RotateAround(transform.position, Vector3.back, angleRotation);
+            }
+            diley = auxDiley;
+        }
+    }
+}
---
+++ Snake-Clon_TheoLeyenda/Assets/Scripts/Obstaculos/Trap1.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Trap1 : MonoBehaviour {
+
+    // Use this for initialization
+    public float timeActive;
+    public float timeDisable;
+    private float auxTimeActive;
+    private float auxTimeDisable;
+    public bool enterTimeActive;
+    private bool enterTimeDisable;
+	void Start () {
+        auxTimeActive = timeActive;
+        auxTimeDisable = timeDisable;
+        if (enterTimeActive == false)
+        {
+            enterTimeDisable = true;
+        }
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        CheckTrap();
+        Debug.Log(gameObject.tag);
+	}
+    public void CheckTrap()
+    {
+        if(timeActive > 0 && enterTimeActive)
+        {
+            gameObject.tag = "limite";
+            timeActive = timeActive - Time.deltaTime;
+            timeDisable = 0;
+        }
+        if(timeActive <= 0 && enterTimeActive)
+        {
+            enterTimeDisable = true;
+            enterTimeActive = false;
+            timeDisable = auxTimeDisable;
+        }
+        if(timeDisable > 0 && enterTimeDisable)
+        {
+            gameObject.tag = "pasto";
+            timeDisable = timeDisable - Time.deltaTime;
+            timeActive = 0;
+        }
+        if(timeDisable <= 0 && enterTimeDisable)
+        {
+            enterTimeDisable = false;
+            enterTimeActive = true;
+            timeActive = auxTimeActive;
+        }
+    }
+        
+}
